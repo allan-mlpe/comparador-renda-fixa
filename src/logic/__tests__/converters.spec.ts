fca2bbd4144721054fd82b35d0d306850a5dc024
allan-mlpe <allan_mlpe@hotmail.com>
2022-04-09 21:26:08 -0300

    test: refactoring unit tests

--- src/logic/__tests__/converters.spec.ts
+++ src/logic/__tests__/converters.spec.ts
@@ -8,40 +8,17 @@ import {
     ipcaLciToPrefixed
 } from "../converters";
 
-const cdbToLciData = [
-    { input: { cdbFee: 110, incomeTax: 0.175 }, result: 90.75 },
-    { input: { cdbFee: 0, incomeTax: 0.175 }, result: 0 },
-    { input: { cdbFee: 100, incomeTax: 0.225 }, result: 77.5 },
-    { input: { cdbFee: 108, incomeTax: 0.20 }, result: 86.4 },
-    { input: { cdbFee: 140, incomeTax: 0 }, result: 140 }
-]
-
-const lciToCdbData = [
-    { input: { lciFee: 90.75, incomeTax: 0.175 }, result: 110 },
-    { input: { lciFee: 0, incomeTax: 0.175 }, result: 0 },
-    { input: { lciFee: 86.4, incomeTax: 0.20 }, result: 108 },
-    { input: { lciFee: 108, incomeTax: 0.50 }, result: 216 },
-    { input: { lciFee: 140, incomeTax: 1 }, result: 0 }
-]
-
-const postFixedCdbToPrefixedData = [
-    { input: { postFixedCdbFee: 100, interestRate: 0.12 }, result: 12 },
-    { input: { postFixedCdbFee: 0, interestRate: 0.12 }, result: 0 },
-    { input: { postFixedCdbFee: 119, interestRate: 0.11 }, result: 13.09 },
-    { input: { postFixedCdbFee: 300, interestRate: 0 }, result: 0 }
-]
-
-const postFixedLciToPrefixedData = [
-    { input: { postFixedLciFee: 109, incomeTax: 0.175, interestRate: 0.12 }, result: 10.790999999999999 },
-    { input: { postFixedLciFee: 0, incomeTax: 0.175, interestRate: 0.08 }, result: 0 },
-    { input: { postFixedLciFee: 115, incomeTax: 0.225, interestRate: 0.11 }, result: 9.80375 },
-    { input: { postFixedLciFee: 100, incomeTax: 0.15, interestRate: 0.14 }, result: 11.9 },
-    { input: { postFixedLciFee: 122, incomeTax: 0, interestRate: 0.04 }, result: 4.88 }
-]
-
 describe("Converters", () => {
     it("Converts a CDB to LCI/LCA properly", () => {
-        cdbToLciData.forEach(({ input, result }) => {
+        const testData = [
+            { input: { cdbFee: 110, incomeTax: 0.175 }, result: 90.75 },
+            { input: { cdbFee: 0, incomeTax: 0.175 }, result: 0 },
+            { input: { cdbFee: 100, incomeTax: 0.225 }, result: 77.5 },
+            { input: { cdbFee: 108, incomeTax: 0.20 }, result: 86.4 },
+            { input: { cdbFee: 140, incomeTax: 0 }, result: 140 }
+        ]
+
+        testData.forEach(({ input, result }) => {
             const lci = cdbToLci(input.cdbFee, input.incomeTax);
 
             expect(lci).toBe(result);
@@ -49,7 +26,15 @@ describe("Converters", () => {
     })
 
     it("Converts a LCI/LCA to CDB properly", () => {
-        lciToCdbData.forEach(({ input, result }) => {
+        const testData = [
+            { input: { lciFee: 90.75, incomeTax: 0.175 }, result: 110 },
+            { input: { lciFee: 0, incomeTax: 0.175 }, result: 0 },
+            { input: { lciFee: 86.4, incomeTax: 0.20 }, result: 108 },
+            { input: { lciFee: 108, incomeTax: 0.50 }, result: 216 },
+            { input: { lciFee: 140, incomeTax: 1 }, result: 0 }
+        ]
+
+        testData.forEach(({ input, result }) => {
             const cdb = lciToCdb(input.lciFee, input.incomeTax);
 
             expect(cdb).toBe(result);
@@ -57,7 +42,14 @@ describe("Converters", () => {
     })
 
     it("Converts a CDB post fixed to a CDB prefixed properly", () => {
-        postFixedCdbToPrefixedData.forEach(({ input, result }) => {
+        const testData = [
+            { input: { postFixedCdbFee: 100, interestRate: 0.12 }, result: 12 },
+            { input: { postFixedCdbFee: 0, interestRate: 0.12 }, result: 0 },
+            { input: { postFixedCdbFee: 119, interestRate: 0.11 }, result: 13.09 },
+            { input: { postFixedCdbFee: 300, interestRate: 0 }, result: 0 }
+        ]
+
+        testData.forEach(({ input, result }) => {
             const prefixedCdb = postFixedCdbToPrefixed(input.postFixedCdbFee, input.interestRate);
 
             expect(prefixedCdb).toBe(result);
@@ -65,15 +57,23 @@ describe("Converters", () => {
     })
 
     it("Converts a LCI post fixed to a LCI prefixed properly", () => {
-        postFixedLciToPrefixedData.forEach(({ input, result }) => {
+        const testData = [
+            { input: { postFixedLciFee: 109, incomeTax: 0.175, interestRate: 0.12 }, result: 10.79 },
+            { input: { postFixedLciFee: 0, incomeTax: 0.175, interestRate: 0.08 }, result: 0 },
+            { input: { postFixedLciFee: 115, incomeTax: 0.225, interestRate: 0.11 }, result: 9.80 },
+            { input: { postFixedLciFee: 100, incomeTax: 0.15, interestRate: 0.14 }, result: 11.9 },
+            { input: { postFixedLciFee: 122, incomeTax: 0, interestRate: 0.04 }, result: 4.88 }
+        ]
+
+        testData.forEach(({ input, result }) => {
             const prefixedLci = postFixedLciToPrefixed(input.postFixedLciFee, input.incomeTax, input.interestRate);
 
-            expect(prefixedLci).toBe(result);
+            expect(prefixedLci).toBeCloseTo(result, 2);
         })
     })
 
     it("Converts a CDB indexed to IPCA to a prefixed one properly", () => {
-        const ipcaCdbToPrefixedData = [
+        const testData = [
             { input: { cdbFee: 6.02, inflationRate: 0.0667 }, result: 13.09 },
             { input: { cdbFee: 3.87, inflationRate: 0.0667 }, result: 10.80 },
             { input: { cdbFee: 4, inflationRate: 0.11 }, result: 15.44 },
@@ -81,7 +81,7 @@ describe("Converters", () => {
             { input: { cdbFee: 0, inflationRate: 0.0667 }, result: 6.67 }
         ]
 
-        ipcaCdbToPrefixedData.forEach(({ input, result }) => {
+        testData.forEach(({ input, result }) => {
             const cdb = ipcaCdbToPrefixed(input.cdbFee, input.inflationRate);
 
             expect(cdb).toBeCloseTo(result, 2);
