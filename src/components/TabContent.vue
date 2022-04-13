13230e3180d17fd82ff8e6219a5acd9ba76e2cdb
allan-mlpe <allan_mlpe@hotmail.com>
2022-04-12 21:03:10 -0300

    feat: adding the TabContent component

---
+++ src/components/TabContent.vue
@@ -0,0 +1,30 @@
+<script>
+import SimpleCol from "../components/SimpleCol.vue";
+
+export default {
+  components: {
+      SimpleCol
+  },
+  props: {
+      id: String,
+      isActive: {
+          type: Boolean,
+          default: false
+      }
+  },
+};
+</script>
+
+<template>
+  <div
+    class="tab-pane fade show"
+    :class="{ active: isActive }"
+    role="tabpanel"
+    :id="id"
+    :aria-labelledby="`${id}-tab`"
+  >
+    <SimpleCol>
+      <slot></slot>
+    </SimpleCol>
+  </div>
+</template>
---
+++ src/components/__tests__/TabContent.spec.js
@@ -0,0 +1,50 @@
+import { describe, it, expect } from "vitest";
+
+import { mount } from '@vue/test-utils'
+import TabContent from '../TabContent.vue'
+
+describe("TabContent", () => {
+    it("renders properly", () => {
+        const wrapper = mount(TabContent, {
+            props: {
+                id: "tab"
+            }
+        })
+
+        expect(wrapper.find("div#tab").exists()).toBeTruthy()
+    })
+
+    it("renders the slot properly", () => {
+        const wrapper = mount(TabContent, {
+            props: {
+                id: "tab"
+            },
+            slots: {
+                default: `<div id="slot-content">SLOT CONTENT</div>"`
+            }
+        })
+
+        expect(wrapper.find("div#slot-content").exists()).toBeTruthy()
+    })
+
+    it("renders an not active tab as default", () => {
+        const wrapper = mount(TabContent, {
+            props: {
+                id: "desactive-tab"
+            }
+        })
+
+        expect(wrapper.classes('active')).toBe(false)
+    })
+
+    it("renders an active tab when passing `isActive` prop as true", () => {
+        const wrapper = mount(TabContent, {
+            props: {
+                id: "active-tab",
+                isActive: true
+            }
+        })
+
+        expect(wrapper.classes('active')).toBe(true)
+    })
+})
