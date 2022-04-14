5c5a943861dc8cf655737d056a1253d44bf9003d
allan-mlpe <allan_mlpe@hotmail.com>
2022-04-13 21:52:16 -0300

    feat: adding TabItem component

---
+++ src/components/TabItem.vue
@@ -0,0 +1,33 @@
+<script>
+export default {
+  props: {
+    contentId: String,
+    tabTitle: {
+        type: String,
+        required: true,
+    },
+    isActive: {
+      type: Boolean,
+      default: false,
+    },
+  },
+};
+</script>
+
+<template>
+  <li class="nav-item" role="presentation">
+    <button
+      class="nav-link"
+      :class="{ active: isActive }"
+      :id="`${contentId}-tab`"
+      data-bs-toggle="tab"
+      :data-bs-target="`#${contentId}`"
+      type="button"
+      role="tab"
+      :aria-controls="contentId"
+      :aria-selected="isActive"
+    >
+      {{ tabTitle }}
+    </button>
+  </li>
+</template>
