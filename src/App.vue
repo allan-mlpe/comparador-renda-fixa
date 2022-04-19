1fab0a692f88b74abebf532dd40b1f2bebdaa80e
allan-mlpe <allan_mlpe@hotmail.com>
2022-04-19 20:31:54 -0300

    refactor: cleaning App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,14 @@
 <script setup>
 import TheHeader from "./components/TheHeader.vue";
 import TheFooter from "./components/TheFooter.vue";
-import TabItem from "./components/TabItem.vue";
-import PrefixedInvestment from "./views/PrefixedInvestment.vue";
-import PostFixedInvestment from "./views/PostFixedInvestment.vue";
-import PostFixedIpcaInvestment from "./views/PostFixedIpcaInvestment.vue";
+import MainTabs from "./views/MainTabs.vue"
 </script>
 
 <template>
   <TheHeader />
 
   <main>
-    <ul class="nav nav-tabs nav-fill" id="myTab" role="tablist">
-      <TabItem contentId="prefixed" tabTitle="Prefixado" :isActive="true" />
-      <TabItem contentId="postfixed" tabTitle="Pós-fixado" />
-      <TabItem contentId="postfixed-ipca" tabTitle="IPCA" />
-    </ul>
-    <div class="tab-content mt-3" id="myTabContent">
-      <PrefixedInvestment />
-      <PostFixedInvestment />
-      <PostFixedIpcaInvestment />
-    </div>
+    <MainTabs />
   </main>
 
   <TheFooter />
---
+++ src/views/MainTabs.vue
@@ -0,0 +1,28 @@
+<script>
+import PrefixedInvestment from "./PrefixedInvestment.vue";
+import PostFixedInvestment from "./PostFixedInvestment.vue";
+import PostFixedIpcaInvestment from "./PostFixedIpcaInvestment.vue";
+import TabItem from "../components/TabItem.vue";
+
+export default {
+    components: {
+        PrefixedInvestment,
+        PostFixedInvestment,
+        PostFixedIpcaInvestment,
+        TabItem
+    }
+}
+</script>
+
+<template>
+  <ul class="nav nav-tabs nav-fill" id="investment-tabs" role="tablist">
+    <TabItem contentId="prefixed" tabTitle="Prefixado" :isActive="true" />
+    <TabItem contentId="postfixed" tabTitle="Pós-fixado" />
+    <TabItem contentId="postfixed-ipca" tabTitle="IPCA" />
+  </ul>
+  <div class="tab-content mt-3" id="investment-tabs-content">
+    <PrefixedInvestment />
+    <PostFixedInvestment />
+    <PostFixedIpcaInvestment />
+  </div>
+</template>
